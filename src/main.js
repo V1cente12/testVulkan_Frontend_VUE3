aa727aba463e67ac0c0c61d1fad2f74da82d5851
V1cente12 <jvicentecv@gmail.com>
2025-01-11 23:02:40 -0400

    notifications  toast

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 axios.interceptors.request.use((config) => {
   const username = localStorage.getItem('username');
@@ -16,4 +18,5 @@ axios.interceptors.request.use((config) => {
 
 const app = createApp(App);
 app.use(router);
+app.use(Toast);
 app.mount('#app');
