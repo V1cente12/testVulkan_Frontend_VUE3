d8db1640fcd451f79e4fe8da4133823b4744ce3b
V1cente12 <jvicentecv@gmail.com>
2025-01-11 23:03:15 -0400

    update routes to statistics, courses and students

--- src/router.js
+++ src/router.js
@@ -1,13 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from './components/Login.vue';
 import Dashboard from './components/Dashboard.vue';
+import Statistics from './components/Statistics.vue'; 
+import Courses from './components/Courses.vue';
+import Students from './components/Students.vue';
 
 const routes = [
-  { path: '/', component: Login },  
+  { path: '/', component: Login },
   { 
-    path: '/dashboard', 
-    component: Dashboard, 
-    meta: { requiresAuth: true }, 
+    path: '/dashboard',
+    component: Dashboard,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: 'statistics', 
+        name: 'statistics',
+        component: Statistics,
+      },
+      {
+        path: 'courses', 
+        name: 'courses',
+        component: Courses,
+      },
+      {
+        path: 'students', 
+        name: 'students',
+        component: Students,
+      },
+    ]
   },
 ];
 
@@ -16,19 +36,18 @@ const router = createRouter({
   routes,
 });
 
-
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth) {
     const username = localStorage.getItem('username');
     const password = localStorage.getItem('password');
-
+    
     if (!username || !password) {
-      next('/'); 
+      next('/');
     } else {
-      next(); 
+      next();
     }
   } else {
-    next(); 
+    next();
   }
 });
 
